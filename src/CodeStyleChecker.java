88b535793c636b9cf1ed95e15d4c550231129c0b
Riad Zaid <r.zaid@student.tudelft.nl>
2024-04-26 19:04:09 +0200

    Getting name of methods with same line open curly bracket

--- src/CodeStyleChecker.java
+++ src/CodeStyleChecker.java
@@ -1,9 +1,17 @@
 import java.io.BufferedReader;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CodeStyleChecker {
     private BufferedReader reader;
+    /**
+     * List of keywords that are banned in the code when checking for camelCase in a *method* name.
+     */
+    private List<String> bannedKeywords = List.of(
+            "if", "class", "while", "for", "switch", "case", "default"
+    );
 
     /**
      * Constructor for CodeStyleChecker
@@ -21,6 +29,38 @@ public class CodeStyleChecker {
      */
     public double checkCamelCase() {
         List<String> lines = reader.lines().toList();
+        int totalMethods = 0;
+        int methodsNotInCamelCase = 0;
+
+        for (int i =0; i<lines.size();i++){
+            //Iterate over each line.
+            String line = lines.get(i); // current line
+
+            //I will handle 2 cases here:
+            //1. Opening Curly bracket on same line. Eg: public void fooBar(){...}
+            //2. Opening Curly bracket on the next line. Eg: public void fooBar()
+            //                                                  {...}
+
+            //Case 1:
+            if(line.contains("{")){
+                List<String> words = Arrays.stream(line.split(" ")).toList();
+                //We check if it is a forloop, if statement, switch, class declaration or method declaration.
+                //HANDLE OTHER KEYWORDS HERE.
+                if(bannedKeywords.stream().anyMatch(words::contains))continue;
+                //We will check if the method name is in camelCase.
+                String methodName = words.getLast().replace("{","").replace("(","").replace(")","").replace("}","");
+                totalMethods++;
+                if(!isCamelCase(methodName)){
+                    //Method name is not in camelCase.
+                    System.out.println("Method name is not in camelCase at line "+ i);
+                    methodsNotInCamelCase++;
+                }
+            }
+        }
+
+        System.out.println("Total methods: "+totalMethods);
+        System.out.println("Methods not in camelCase: "+methodsNotInCamelCase);
+        return (methodsNotInCamelCase*100.0)/totalMethods;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = null;
+        BufferedReader reader = null;
         try {
-            scanner = new Scanner(new File("./FakeCode/Main.java"));
+            reader = new BufferedReader(new java.io.FileReader(new File("./FakeCode/Main.java")));
         } catch (Exception e) {
             System.out.println("File not found.");
+            return;
         }
-        if (scanner == null) return;
 
         // CodeStyleChecker currently validates only camelCase.
-        CodeStyleChecker codeStyleChecker = new CodeStyleChecker(scanner);
+        CodeStyleChecker codeStyleChecker = new CodeStyleChecker(reader);
         double percentageThatDoesNotAdhere = codeStyleChecker.checkCamelCase();
 
         System.out.println(percentageThatDoesNotAdhere);
-
-        scanner.close();
     }
 }
